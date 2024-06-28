0837c7db9139c0ed62f035e7c6e2255dd56f3228
nastenka-ooops <nastenka.brutskaya@mail.ru>
2024-06-28 10:56:36 +0300

    update far file

--- src/main/java/com/neotour/config/OpenApiConfig.java
+++ src/main/java/com/neotour/config/OpenApiConfig.java
@@ -2,6 +2,7 @@ package com.neotour.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,7 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .addServersItem(new Server().url("https://auth-project-production.up.railway.app"))
                 .info(new Info()
                         .title("NeoTour API")
                         .version("1.0")
